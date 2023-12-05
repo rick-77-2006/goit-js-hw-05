62b79b6d797440c4b975bf3b36c353844c068160
Rick <46992687+rick-77-2006@users.noreply.github.com>
2023-12-05 13:53:40 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -3,45 +3,46 @@ const getTotalBalanceByGender = (users, gender) => {
     if (usr.gender != gender) return totalBalance;
     return totalBalance + usr.balance;
   }, 0);
-};
+  };
 
-const allUsers = [
-  {
+  const allUsers = [
+    {
     name: "Moore Hensley",
     gender: "male",
-    balance: 2811,
+    balance: 2811
   },
   {
     name: "Sharlene Bush",
     gender: "female",
-    balance: 3821,
+    balance: 3821
   },
   {
     name: "Ross Vazquez",
     gender: "male",
-    balance: 3793,
+    balance: 3793
   },
   {
     name: "Elma Head",
     gender: "female",
-    balance: 2278,
+    balance: 2278
   },
   {
     name: "Carey Barr",
     gender: "male",
-    balance: 3951,
+    balance: 3951
   },
   {
     name: "Blackburn Dotson",
     gender: "male",
-    balance: 1498,
+    balance: 1498
   },
   {
     name: "Sheree Anthony",
     gender: "female",
-    balance: 2764,
-  },
+    balance: 2764
+  }
 ];
 
 console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+
 console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
